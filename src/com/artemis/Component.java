5efc5b287891cdd3fef0d586c30da0da972a5f01
Michael McCarron <firedem0n@hotmail.com>
2013-12-15 16:07:14 -0700

    Adds artemis sources.

---
+++ src/com/artemis/Component.java
@@ -0,0 +1,12 @@
+package com.artemis;
+
+import java.io.Serializable;
+
+/**
+ * A tag class. All components in the system must extend this class.
+ * 
+ * @author Arni Arent
+ */
+public abstract class Component implements Serializable 
+{
+}
